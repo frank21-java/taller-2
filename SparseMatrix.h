afeea3969fba2977d76f81af57a2ca03bf2f00f1
frank21-java <francisco.tegnologico@gmail.com>
2025-10-15 18:03:06 -0300

    Rename remove method to remover
    Renamed 'remove' method to 'remover' in SparseMatrix class.

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -7,9 +7,10 @@ class SparseMatrix {
         SparseMatrix();
         void add(int value, int xPos, int yPos);
         int get(int xPos, int yPos);
-        void remove(int xPos, int yPos);
+        int remover(int xPos, int yPos);
         void printStoredValues();
         int density();
         SparseMatrix* multiply(SparseMatrix* second);
         ~SparseMatrix();
+
 };
